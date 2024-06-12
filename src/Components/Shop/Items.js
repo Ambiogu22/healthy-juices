96f17dd273ed4f59e979882a3e3c2985ae5569b8
Ambiogu22 <ambio22620@gmail.com>
2024-06-12 15:20:26 -0400

    V0.5 (Changes on Shop)

---
+++ src/Components/Shop/Items.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const itemsArray = [
+    {
+        id:1,
+        image:"https://cdn-icons-png.flaticon.com/512/71/71879.png",
+        title:"Green Juice",
+        description: "Our aim is to seamlessly integrate into your daily routine, offering time-saving solutions while prioritizing your well-being. Our products are designed to combat inflammation, detoxify, and cleanse your body and skin, ensuring you feel rejuvenated and refreshed each day",
+        price: "19.99"
+    },
+    {
+        id:2,
+        image:"https://cdn-icons-png.flaticon.com/512/71/71879.png",
+        title:"Orange Juice",
+        description: "Our aim is to seamlessly integrate into your daily routine, offering time-saving solutions while prioritizing your well-being. Our products are designed to combat inflammation, detoxify, and cleanse your body and skin, ensuring you feel rejuvenated and refreshed each day",
+        price: "199.99"
+    }
+]
+
+const getItemData = (id) => {
+    let itemData = itemsArray.find(item => item.id === id)
+
+    if (itemData == undefined){
+        console.log(`The product with id:${id} doen't exist.`)
+    }
+
+    return itemData;
+}
+
+export {itemsArray, getItemData}
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,23 +1,24 @@
 import React from "react";
-import Item from "./Item";
+import { itemsArray } from "./Items";
 
 const Shop = () => {
     return (
         <div className="flex flex-col mx-16">
             <h2 className="mt-32 pb-32 text-7xl font-semibold">Healthy Juices</h2>
             <div className="flex flex-col gap-12 justify-between items-center">
-                <Item
-                img="https://cdn-icons-png.flaticon.com/512/71/71879.png" 
-                imgAlt="Green Juice Image"
-                title="Green Juice"
-                price="19.99"
-                />
-                <Item
-                img="https://cdn-icons-png.flaticon.com/512/71/71879.png" 
-                imgAlt="Orange Juice Image"
-                title="Orange Juice"
-                price="19.99"
-                />
+                {itemsArray.map((item) => (
+                            <div className="flex flex-col gap-12 items-center bg-[#F9F7F1] p-8 rounded-2xl py-24 px-24 mb-24">
+                                <img src={item.image} alt={item.imgAlt} className="w-56"/>
+                                <h2 className="text-5xl font-semibold">{item.title}</h2>
+                                <div className="flex flex-col gap-4 items-center">
+                                    <h2 className="text-5xl font-medium">${item.price}</h2>
+                                    <h2 className="text-3xl text-[#6B6B6B]">Week</h2>
+                                </div>
+                                <div className="flex justify-center items-center">
+                                    <button className="text-4xl font-semibold py-6 px-12 border-2 border-[#000] rounded-full">ADD TO CART</button>
+                                </div>
+                            </div>
+                ))}
             </div>
         </div>
     );
