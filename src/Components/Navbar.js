abceb891d50c5a952dff8353423b441fe258fa8f
Ambiogu22 <ambio22620@gmail.com>
2024-06-18 14:50:40 -0400

    V0.12 (Qty of items on cartLogo done!)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping, faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react";
+import { useContext } from "react";
+import { CartContext } from "./Shop/ShopContext";
 
 const Navbar = () => {
+
+    const cart = useContext(CartContext);
+
+    const itemsInCart = cart.getTotalItems();
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -40,7 +46,12 @@ const Navbar = () => {
                 </div>}
                 {!isOpen &&
                 <ul>
-                    <li><Link to="/cart" onClick={shoppingCartToggle}><FontAwesomeIcon icon={faCartShopping}/></Link></li>
+                    <div className="relative">
+                        {itemsInCart >= 1 &&
+                            <h2 className="absolute text-[#FFF] bg-[#FF0000] rounded-full text-[30px] px-[14px] py-[6px] font-bold right-[-20px] top-[-20px]">{itemsInCart}</h2>
+                        }
+                        <li><Link to="/cart" onClick={shoppingCartToggle}><FontAwesomeIcon icon={faCartShopping}/></Link></li>
+                    </div>
                 </ul>
                 }
             </div>  
